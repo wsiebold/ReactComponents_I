0b2986f150ad4dccbcdbb270f9a490b4d4a31e49
wsiebold <email@designpdx.com>
2021-06-28 13:24:37 -0700

    Initial commit

---
+++ src/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Wondering Aloud</li>
+      <li>Hymn 43</li>
+      <li>Teacher</li>
+      <li>Elegy</li>
+      <li>Life is a Long Song</li>
+    </ul>
+  );
+}
+
+export default List;
